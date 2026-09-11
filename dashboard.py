a135fa6bbef0323585ee60021fea46186b793a0c
mylenaizidoro <izidoromylena@gmail.com>
2026-09-11 09:12:16 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -13,7 +13,7 @@ cards_placeholder = st.empty()
 grafico_placeholder = st.empty()
 
 # URL atualizada com o novo túnel ativo do Serveo
-API_GET_URL = "https://5f132bb71ff09a48-179-68-117-40.serveousercontent.com/temperaturas"
+API_GET_URL = "https://ninety-actors-read.loca.lt/temperaturas"
 
 # Cabeçalho necessário para o Serveo liberar o acesso da API sem a página de aviso
 HEADERS = {"serveo-skip-browser-warning": "true"}
